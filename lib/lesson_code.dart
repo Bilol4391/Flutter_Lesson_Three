53d94b32c65e9e8baa137948c85433fada45dc4d
Bilol Abdunazarov <bilolabdunazarov8480gmail.com@Bilols-MacBook-Pro.local>
2022-11-09 18:03:53 +0500

    Exercise - 1

---
+++ lib/lesson_code.dart
@@ -0,0 +1,39 @@
+// This widget is the root of your application.
+import 'package:flutter/material.dart';
+
+class LessonCode extends StatelessWidget {
+  const LessonCode({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        color: Colors.white,
+        child: Center(
+          child: Container(
+            width: 200,
+            height: 100,
+            child: Center(
+              child: Text(
+              "my widget",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  decoration: TextDecoration.none, color: Colors.black, fontWeight: FontWeight.w400, fontSize: 30),
+            ),
+          ),
+            decoration: BoxDecoration(
+              border: Border.all(
+                  width: 3.0,
+                color: Colors.blue,
+              ),
+              borderRadius: BorderRadius.all(
+                  Radius.circular(30) //
+              ),
+            ),
+          ),
+        ),
+
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_lesson_three/lesson_code.dart';
+
+
+void main() {
+  runApp(const LessonCode());
+}
